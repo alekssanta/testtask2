b5daa1a9585205839ea0b5a84c085c77af86f8da
Lapin Aleksandr <Aleksandr.Lapin@homecredit.ru>
2021-12-08 15:47:14 +0400

    1st commit

---
+++ src/test/java/Animals.java
@@ -0,0 +1,31 @@
+public class Animals {
+    void eat (Food food){
+
+    }
+
+}
+class herbivore extends  Animals{
+
+}
+
+class carnovorous extends Animals{
+
+}
+class duck extends herbivore{
+
+}
+class elephant extends herbivore{
+
+}
+class rabbit extends  herbivore{
+
+}
+class wolf extends carnovorous{
+
+}
+class fox extends carnovorous{
+
+}
+class shark extends carnovorous{
+
+}
---
+++ src/test/java/Food.java
@@ -0,0 +1,21 @@
+public class Food {
+}
+
+class Grass extends Food{
+}
+class Meat extends Food{
+}
+class  dill extends Grass{
+}
+class parsley extends Grass{
+}
+class onion extends Grass{
+}
+
+class pork extends Meat{
+}
+class chickenMeat extends Meat {
+}
+class beef extends Meat{
+}
+
